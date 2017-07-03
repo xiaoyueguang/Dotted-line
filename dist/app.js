67f2b2ebc5c26df0d51e9ca87bf6bcd352d1006a
linjilei <linjilei@qq.com>
2017-07-03 14:38:05 +0800

    添加 demo

--- dist/app.js
+++ dist/app.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -95,7 +95,75 @@ let height = parseInt(bodyHeight, 10)
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__pointer_js__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__helper_js__ = __webpack_require__(0);
+
+
+// 点 对象
+class Pointer {
+  constructor (width, height, r, time = 5, color = '#f3f3f3') {
+    this.width = width
+    this.height = height
+    let x = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(width)
+    let y = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(height)
+    this.x = x
+    this.y = y
+    this.origin = {x, y}
+
+    this.color = color
+    // 最小为 10
+    this.r = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(r, 1)
+    this.time = time * 1000
+    this.targetInit()
+    this.isStart = true
+  }
+  // 设置开始时间
+  setStart () {
+    this.startTime = new Date().getTime()
+    this.endTime = this.startTime + this.time
+  }
+
+  // 生成目标点
+  targetInit () {
+    // 当已经开始时, 需要设置原先的点
+    if (this.isStart) {
+      this.origin = {
+        x: this.targetX,
+        y: this.targetY
+      }
+    }
+    this.targetX = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(this.width)
+    this.targetY = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(this.height)
+    this.setStart()
+  }
+  // 获取当前进度百分比
+  getPercent () {
+    let now = new Date().getTime()
+    let percent = Math.floor((now - this.startTime) / this.time * 10000) / 100
+    return percent > 100 ? 100 : percent
+  }
+
+  move (pointer, targetPointer) {
+    let tween = this.getPercent()
+    return targetPointer > pointer ?
+      pointer + Math.abs(targetPointer - pointer) * tween / 100 :
+      pointer - Math.abs(targetPointer - pointer) * tween / 100
+  }
+
+  run () {
+    this.x = this.move(this.origin.x, this.targetX)
+    this.y = this.move(this.origin.y, this.targetY)
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Pointer;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__pointer_js__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__helper__ = __webpack_require__(0);
 /**
  * 舞台
@@ -260,176 +328,7 @@ class Canvas {
   }
 }
 
-/* harmony default export */ __webpack_exports__["a"] = (Canvas);
-
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/**
- * 补间动画
- * 返回的方法.
- * 初始化 传入 一个 起点和终点.
- * 根据 贝塞尔曲线. 返回一个 方法,
- * 该方法接受一个 0 - 100 的数字
- * 返回在某个点时 应该所在的数字.
- * 
- * 具体实现: 待调整
- * linear 比例大概是 每次 都是为1
- * ease 为
- */
-
-/**
- * linear
- * @param {number} start 起点
- * @param {number} end 终点
- * @param {number} percent 百分比数字 0-100
- * @return {number} 返回某个百分比下的位置
- */
-const linear = (...rest) => calc(1, ...rest)
-
-/**
- * ease
- * TODO: 错误
- * @param {number} start 起点
- * @param {number} end 终点
- * @param {number} percent 百分比数字 0-100
- * @return {number} 返回某个百分比下的位置
- * (x-50)(x-50) = y + 2400
- */
-const ease = (...rest) => {
-  const [start, end, percent] = rest
-  return calc(Math.sqrt(percent + 2400) + 50, ...rest)
-}
-/**
- * 计算
- * @param {number} bezier 贝塞尔系数
- * @param {number} start 起点
- * @param {number} end 终点
- * @param {number} percent 百分比数字 0-100
- * @return {number} 返回某个百分比下的位置
- */
-const calc = (bezier, start, end, percent) => {
-  let distance = Math.abs(start - end) * percent / 100 * bezier
-  return start > end ?
-    start - distance : start + distance
-}
-
-/* unused harmony default export */ var _unused_webpack_default_export = ({
-  ease, linear
-});
-
-
-/***/ }),
-/* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__stage_js__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__helper__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__tween_js__ = __webpack_require__(2);
-
-
-
-
-for (let i = 0; i < 1; i++) {
-  window[`canvas${i}`] = new __WEBPACK_IMPORTED_MODULE_0__stage_js__["a" /* default */]({
-    el: document.getElementById(`canvas${i}`),
-    width: 400,
-    height: 400,
-    limit: 15,
-    time: 60
-  })
-}
-
-setInterval(() => {
-  // let canvas = window[`canvas${random(1)}`]
-  // let canvas = window.canvas0
-  // random(2) > 1 ? canvas.addPointer() : canvas.delPointer()
-}, 1000)
-
-setInterval(() => {
-  let canvas = window.canvas0
-  canvas.pointers.forEach(pointer => {
-    pointer.targetInit()
-  })
-}, 60000)
-
-// window.canvas = new Canvas({
-//   el: document.getElementById('canvas0'),
-//   width: 400,
-//   height: 400,
-//   limit: 1
-// })
-
-/***/ }),
-/* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__helper_js__ = __webpack_require__(0);
-
-
-// 点 对象
-class Pointer {
-  constructor (width, height, r, time = 5, color = '#f3f3f3') {
-    this.width = width
-    this.height = height
-    let x = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(width)
-    let y = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(height)
-    this.x = x
-    this.y = y
-    this.origin = {x, y}
-
-    this.color = color
-    // 最小为 10
-    this.r = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(r, 1)
-    this.time = time * 1000
-    this.targetInit()
-    this.isStart = true
-  }
-  // 设置开始时间
-  setStart () {
-    this.startTime = new Date().getTime()
-    this.endTime = this.startTime + this.time
-  }
-
-  // 生成目标点
-  targetInit () {
-    // 当已经开始时, 需要设置原先的点
-    if (this.isStart) {
-      this.origin = {
-        x: this.targetX,
-        y: this.targetY
-      }
-    }
-    this.targetX = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(this.width)
-    this.targetY = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helper_js__["c" /* random */])(this.height)
-    this.setStart()
-  }
-  // 获取当前进度百分比
-  getPercent () {
-    let now = new Date().getTime()
-    let percent = Math.floor((now - this.startTime) / this.time * 10000) / 100
-    return percent > 100 ? 100 : percent
-  }
-
-  move (pointer, targetPointer) {
-    let tween = this.getPercent()
-    return targetPointer > pointer ?
-      pointer + Math.abs(targetPointer - pointer) * tween / 100 :
-      pointer - Math.abs(targetPointer - pointer) * tween / 100
-  }
-
-  run () {
-    this.x = this.move(this.origin.x, this.targetX)
-    this.y = this.move(this.origin.y, this.targetY)
-  }
-}
-/* harmony export (immutable) */ __webpack_exports__["a"] = Pointer;
-
+/* harmony default export */ __webpack_exports__["default"] = (Canvas);
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,33 +1,163 @@
-import Canvas from './stage.js'
-import {random} from './helper'
-import tween from './tween.js'
+/**
+ * 舞台
+ */
+import Pointer from './pointer.js'
+import {width, height, random} from './helper.js'
 
-for (let i = 0; i < 1; i++) {
-  window[`canvas${i}`] = new Canvas({
-    el: document.getElementById(`canvas${i}`),
-    width: 400,
-    height: 400,
-    limit: 15,
-    time: 60
-  })
+class Canvas {
+  constructor ({
+    el,
+    limit = 10,
+    pointerWidth = 15,
+    width = width,
+    height = height,
+    time = 5,
+    color = ['#f3f3f3']
+  }) {
+    // 防止创建多次
+    this.isInited = false;
+    // 限制点数量
+    this.pointerLimit = limit;
+    // 点的宽度
+    this.pointerWidth = pointerWidth;
+    // 点集合
+    this.pointers = [];
+    // 上下文
+    this.ctx = null;
+    // 元素
+    this.el = null;
+    // 时间
+    this.time = time
+    this.color = color
+
+    this.init(el, width, height)
+
+    this.render()
+  }
+  // 初始化
+  init ($el, width, height) {
+    // 防止初始化两次
+    if (this.isInited) return
+    this.isInited = true
+    // 保存 长宽
+    this.width = width;
+    this.height = height;
+    // 保存原本元素
+    this.el = $el
+    this.el.width = width;
+    this.el.height = height;
+    // 获取上下文
+    this.ctx = this.el.getContext('2d')
+
+    // 画点
+    for (let i = 0; i < this.pointerLimit; i++) {
+      this.pointerInit()
+    }
+
+  }
+
+  // 生成点
+  pointerInit () {
+    let colorLength = this.color.length
+    let pointer = new Pointer(
+      this.width,
+      this.height,
+      this.pointerWidth,
+      this.time,
+      this.color[random(colorLength)]
+    )
+
+    this.pointers.push(pointer)
+    this.pointerRender(pointer)
+  }
+
+  // 画点
+  pointerRender (pointer) {
+    this.ctx.beginPath()
+    this.ctx.arc(
+      pointer.x,
+      pointer.y,
+      pointer.r,
+      0,
+      2 * Math.PI,
+      true
+    )
+    this.ctx.fillStyle = pointer.color
+    this.ctx.fill()
+  }
+  /**
+   * 设置点数限制
+   * @param {number} limit
+   */
+  setLimit (limit) {
+    if (limit < 0) limit = 0
+    let _limit = this.pointers.length
+    if (limit > _limit) {
+      for(let i = 0; i < limit - _limit; i++) {
+        this.pointerInit()
+      }
+    } else {
+      for(let i = 0; i < _limit - limit; i++) {
+        this.pointers.shift()
+      }
+    }
+  }
+  /**
+   * 添加点
+   */
+  addPointer () {
+    this.setLimit(this.pointers.length + 1)
+  }
+  /**
+   * 删除点
+   */
+  delPointer () {
+    this.setLimit(this.pointers.length - 1)
+  }
+
+  // 点跑起来
+  pointerRun () {
+    this.pointers.forEach((pointer) => {
+      pointer.run()
+      this.pointerRender(pointer)
+    })
+  }
+
+  // 画线
+  lineRun () {
+    let pointer = this.pointers
+    let length = pointer.length
+    let ctx = this.ctx
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        if (i !== j) {
+          let pointer1 = pointer[i]
+          let pointer2 = pointer[j]
+          let gradient = ctx.createLinearGradient(pointer1.x, pointer1.y, pointer2.x, pointer2.y)
+          gradient.addColorStop(0, pointer1.color)
+          gradient.addColorStop(1, pointer2.color)
+
+          ctx.beginPath()
+          ctx.moveTo(pointer[i].x, pointer[i].y)
+          ctx.lineTo(pointer[j].x, pointer[j].y)
+
+          ctx.strokeStyle = gradient
+          ctx.stroke()
+        }
+      }
+    }
+  }
+
+  // 画
+  render () {
+    this.ctx.clearRect(0, 0, this.width, this.height)
+    this.ctx.fillStyle = '#f3f3f3'
+    this.ctx.strokeStyle = '#f3f3f3'
+    this.pointerRun()
+    this.lineRun()
+
+    requestAnimationFrame(this.render.bind(this))
+  }
 }
 
-setInterval(() => {
-  // let canvas = window[`canvas${random(1)}`]
-  // let canvas = window.canvas0
-  // random(2) > 1 ? canvas.addPointer() : canvas.delPointer()
-}, 1000)
-
-setInterval(() => {
-  let canvas = window.canvas0
-  canvas.pointers.forEach(pointer => {
-    pointer.targetInit()
-  })
-}, 60000)
-
-// window.canvas = new Canvas({
-//   el: document.getElementById('canvas0'),
-//   width: 400,
-//   height: 400,
-//   limit: 1
-// })
+export default Canvas;
